b390830679dce1dc6e56daf55f2bf424abc0edd2
guillermodaniel2803 <ggarbre2803@g.educaand.es>
2025-04-03 12:59:13 +0200

    Delete TestGestSimAlm.java

--- TestGestSimAlm.java
+++
@@ -1,7 +0,0 @@
-public class TestGestSimAlm {
-    public static void main(String[] args) {
-        GestSimAlm gestion = new GestSimAlm();
-        gestion.entradaAutomatizada();
-        gestion.listar();
-    }
-}
